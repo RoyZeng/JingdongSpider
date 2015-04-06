5c52760ae0830c79b06df82e4fac6c6c0e34e760
jiayiting <jiayiting93@163.com>
2015-04-06 19:56:57 +0800

    这里简要描述你都做什么了

--- src/main/java/net/spider/main.java
+++ src/main/java/net/spider/main.java
@@ -4,6 +4,7 @@ package net.spider;
 public class main {
     //program entry
     public static void main(String[] args) {
+    	
         Spider crawler = new Spider();
         crawler.excuteSpider();
     }
